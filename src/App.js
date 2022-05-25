da8099b74ca044efbb93ea2f62e0a99505f4375f
Jayy <104801658+642phoenix@users.noreply.github.com>
2022-05-25 11:45:31 -0700

    test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
         <h1>Que Rollo BONACHON</h1>
       </header>
 
+      <div id="hero" className="d-flex flex-column justify-content-center align-items-center">
+        <div className="hero-container" data-aos="fade-in">
+          <h1>Alex Smith</h1>
+          <p>I'm <span className="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+        </div>
+      </div>
+      {/* <!-- End Hero --> */}
 
     </div>
   );
